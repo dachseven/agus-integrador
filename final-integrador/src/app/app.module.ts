9d9de037614448868e37b0a5838ce9c91cfa038a
dachseven <oviedoagustin46337@gmail.com>
2023-03-01 12:57:52 -0300

    creacion de los componentes

--- final-integrador/src/app/app.module.ts
+++ final-integrador/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorrarComponent } from './component/borrar/borrar.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { GraficosComponent } from './graficos/graficos/graficos.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { EditarComponent } from './editar/editar/editar.component';
+import { DescripcionComponent } from './descripcion/descripcion/descripcion.component';
+import { LoginComponent } from './login/login/login.component';
+import { LogedComponent } from './loged/loged/loged.component';
+import { CrearComponent } from './crear/crear/crear.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorrarComponent,
+    HeaderComponent,
+    FooterComponent,
+    GraficosComponent,
+    ExperienceComponent,
+    EditarComponent,
+    DescripcionComponent,
+    LoginComponent,
+    LogedComponent,
+    CrearComponent
   ],
   imports: [
     BrowserModule,
---
+++ final-integrador/src/app/loged/loged/loged.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loged',
+  templateUrl: './loged.component.html',
+  styleUrls: ['./loged.component.css']
+})
+export class LogedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
